2849c73ddd18dcb5d8e1aea6d3bbee5519e3b5b3
Lorena Sarasua <lorena.sarasua@gmail.com>
2023-01-11 05:44:30 -0500

    2.5. Studio: Area of a Circle, 2.5.2. Your first task, Area

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println("The area of a circle with radius" + radius + "is: " + area);
+    }
+}
